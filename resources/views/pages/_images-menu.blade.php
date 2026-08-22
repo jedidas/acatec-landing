9be4c24dfdee60a80e0383452cd0f0550e4196c8
David Mora <jedidas@gmail.com>
2026-08-21 18:30:39 -0600

    add content, add menu

--- resources/views/pages/_images-menu.blade.php
+++ resources/views/pages/_images-menu.blade.php
@@ -1,40 +1,43 @@
-<div class="container m-auto -mt-5 relative z-5">
-    <div class="bg-neutral-100/50 rounded-sm flex flex-col lg:flex-row gap-3 lg:gap-5 p-5 pt-7">
+<div class="container m-auto -mt-5 px-4 lg:px-0 relative z-5">
+    <div class="bg-neutral-100/50 rounded-sm flex flex-col lg:flex-row gap-3 lg:gap-5 p-5 pt-7 shadow-md">
         <div class="flex gap-3 lg:gap-5 flex-col md:flex-row lg:flex-col w-full">
-            <div
-                class="bg-primary-700 flex flex-col gap-1 items-center justify-center rounded-sm p-2 lg:p-5 lg:py-8 w-full h-2/5">
-                <x-icon name="capacitaciones" class="block h-10 w-10 fill-white" />
-                <a href=""
-                    class="bg-primary-700 inline-block rounded-sm p-2 font-bold uppercase text-white">Capacitaciones</a>
-            </div>
-            <div
-                class="bg-success-500 flex flex-col gap-1 items-center justify-center rounded-sm p-2 lg:p-5 lg:py-8 w-full h-full">
-                <x-icon name="outsourcing" class="block h-10 w-10 fill-white" /><a href=""
-                    class="bg-success-500 inline-block rounded-sm p-2 font-bold uppercase text-white">Outsourcing</a>
-            </div>
+            <a href=""
+                class="bg-primary-700 image-menu-item image-menu-capacitaciones flex flex-col gap-1 items-center justify-center rounded-sm overflow-hidden p-2 lg:p-5 lg:py-3 w-full h-2/5">
+                <x-icon name="capacitaciones" class="block h-10 w-10 fill-white text-sm lg:text-base" />
+                <span
+                    class="bg-primary-700 inline-block rounded-sm p-1 font-bold uppercase text-white text-sm lg:text-base text-center">Capacitaciones</span>
+            </a>
+            <a href=""
+                class="bg-success-500 image-menu-item image-menu-outsourcing flex flex-col gap-1 items-center justify-center rounded-sm overflow-hidden p-2 lg:p-5 lg:py-3 w-full h-full">
+                <x-icon name="outsourcing" class="block h-10 w-10 fill-white" />
+                <span
+                    class="bg-success-500 inline-block rounded-sm p-1 font-bold uppercase text-white text-sm lg:text-base text-center">Outsourcing</span>
+            </a>
         </div>
         <div class="flex gap-3 lg:gap-5 flex-col md:flex-row lg:flex-col w-full">
-            <div
-                class="bg-neutral-500 flex flex-col gap-1 items-center justify-center rounded-sm p-2 lg:p-5 lg:py-8 w-full h-3/5">
+            <a href=""
+                class="bg-neutral-500 image-menu-item image-menu-planes flex flex-col gap-1 items-center justify-center rounded-sm overflow-hidden p-2 lg:p-5 lg:py-3 w-full h-3/5">
                 <x-icon name="planes" class="block h-10 w-10 fill-white" />
-                <a href=""
-                    class="bg-neutral-500 inline-block rounded-sm p-2 font-bold uppercase text-white">Planes y
-                    programas</a>
-            </div>
-            <div
-                class="bg-info flex flex-col gap-1 items-center justify-center rounded-sm p-2 lg:p-5 lg:py-8 w-full h-2/5">
+                <span
+                    class="bg-neutral-500 inline-block rounded-sm p-1 font-bold uppercase text-white text-sm lg:text-base text-center">Planes
+                    y
+                    programas</span>
+            </a>
+            <a href=""
+                class="bg-info image-menu-item image-menu-bandera flex flex-col gap-1 items-center justify-center rounded-sm overflow-hidden p-2 lg:p-5 lg:py-3 w-full h-2/5">
                 <x-icon name="bandera" class="block h-10 w-10 fill-white" />
-                <a href="" class="bg-info inline-block rounded-sm p-2 font-bold uppercase text-white">Bandera
-                    azul</a>
-            </div>
+                <span
+                    class="bg-info inline-block rounded-sm p-1 font-bold uppercase text-white text-sm lg:text-base text-center">Bandera
+                    azul</span>
+            </a>
         </div>
         <div class="flex gap-3 lg:gap-5 flex-col w-full">
-            <div
-                class="bg-accent-500 flex flex-col gap-1 items-center justify-center rounded-sm p-2 lg:p-5 lg:py-8 h-full w-full">
+            <a href=""
+                class="bg-accent-500 image-menu-item image-menu-servicios flex flex-col gap-1 items-center justify-center rounded-sm overflow-hidden p-2 lg:p-5 lg:py-3 h-full w-full">
                 <x-icon name="servicios" class="block h-10 w-10 fill-white" />
-                <a href=""
-                    class="bg-accent-500 inline-block rounded-sm p-2 font-bold uppercase text-white">Servicios</a>
-            </div>
+                <span
+                    class="bg-accent-500 inline-block rounded-sm p-1 font-bold uppercase text-white text-sm lg:text-base text-center">Servicios</span>
+            </a>
         </div>
     </div>
 </div>
